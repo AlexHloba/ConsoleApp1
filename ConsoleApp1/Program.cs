35461c6f1e6ce353be63f4478a4eec85c8dea1d8
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-06-28 15:15:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("****Карточка****");
+Console.WriteLine("Введите Ваше Имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Введите ваш возраст: ");
+string age = Console.ReadLine();
+Console.WriteLine("Введите ваш рост: ");
+string height = Console.ReadLine();
+Console.WriteLine($"Ваше имя {name} \nВаш возраст {age}\nВаш рост {height} ");
+
+
