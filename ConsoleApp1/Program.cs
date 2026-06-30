148ee9c972deb34d34980b031f93b58479d4d9d0
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-06-30 20:29:35 +0300

    добавил дз3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,3 +20,20 @@ int temp = a;
 a = b; b = temp;
 
 Console.WriteLine($"После обменя значений А равно {a}, В равно {b}");
+
+
+Console.WriteLine("Площадь прямоугольника ");
+
+Console.WriteLine("Введите ширину: ");
+string wid = Console.ReadLine();
+double width = Double.Parse(wid);
+
+Console.WriteLine("Введите длину:");
+string len = Console.ReadLine();
+double length = Double.Parse(len);
+
+double area = width * length;
+double perimetr = 2 * (width + length);
+
+Console.WriteLine($"Площадь прямоугольника равна {area} \nПериметр прямоугольника равен {perimetr}");
+
