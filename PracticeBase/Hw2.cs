561f827cd35d25cdddec816b7ab91937533ecf2e
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-07-12 22:56:15 +0300

    Добавил новый проект

---
+++ PracticeBase/Hw2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeBase
+{
+    public class Hw2
+    {
+        public static void Print1()
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 7; i <= n; i += 7)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ PracticeBase/Hw3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeBase
+{
+    public class Hw3
+    {
+        public static void Print3()
+        {
+            Console.WriteLine("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            while (a < n)
+            {
+                int count = a + b;
+                a = b;
+                b = count;
+
+                Console.Write(a + " ");
+
+            }
+
+
+        }
+    }
+}
