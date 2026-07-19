d90b6e64a4724cac132c552480d7d864a2352275
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-07-19 22:18:19 +0300

    add FileReposiory

---
+++ FinanceTrcker.Infrastructure/Repositories/FileRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Enums;
+
+namespace FinanceTracker.Infrastructure.Repositories
+{
+    public  class FileRepository
+    {
+        public void Add(Transaction transaction)
+        {
+            Console.WriteLine("моя логинка");
+        }
+
+        public List<Transaction> GetAll()
+        {
+            return [
+                new Transaction
+                {
+                    Amount = 4000
+                    Type = TransactionType.Income,
+                    Category = "Зарплата",
+                    Description = "Аванс"
+                },
+                new Transaction
+                {
+                    Amount = 200,
+                    Type = TransactionType.Expense,
+                    Category = "Еда",
+                    Description = "Продукты"
+                }
+
+
+                ];
+        }
+        
+    }
+    
+    
+}
