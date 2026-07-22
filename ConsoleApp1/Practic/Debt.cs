6f623a796fa0ba0cc8fd11c39061abd59be7021e
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-07-22 20:09:59 +0300

    практика

---
+++ ConsoleApp1/Practic/Debt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Practic
+{
+    public class Debt
+    {
+        public double balance;
+        public double interestRate;
+
+
+        public Debt(double initialbalance, double initialInterestRate)
+        {
+            balance = initialbalance;
+            interestRate = initialInterestRate;
+        }
+
+        public void PrintBalance(double balance)
+        {
+            Console.WriteLine(balance);
+        }
+
+        public void WaitOneYear()
+        {
+            balance *= interestRate;
+        }
+
+        public static void Main(string[] args)
+        {
+            Debt mortgage = new Debt(120000.0, 1.01);
+            mortgage.PrintBalance();        
+            mortgage.WaitOneYear();
+            mortgage.PrintBalance();       
+                    
+            int years = 0;
+            while (years < 20)
+            {
+                mortgage.WaitOneYear();
+                years = years + 1;
+            }
+            mortgage.PrintBalance();
+        }
+
+        public void PrintBalance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
