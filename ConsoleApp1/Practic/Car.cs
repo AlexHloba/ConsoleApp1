b0cda15ccfeb0e45985c6b85b040e2af3001d208
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-07-22 22:06:04 +0300

    cars

---
+++ ConsoleApp1/Practic/Car.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1.Practic
+{
+   public interface IVehicle
+    {
+        void Drive(int distance);
+        bool Refuel(int amount);
+    }
+
+    public abstract class Car : IVehicle
+    {
+        public double fuel;
+        public double consumption;
+
+
+        public  Car (double initialFuel, double consumption)
+        {
+            this.fuel = initialFuel;
+            this.consumption = consumption;
+        }
+
+
+        public void Drive(int distance)
+        {
+            if(fuel > 0)
+            {
+                Console.WriteLine("Автомобиль движется");
+            }
+            else
+            {
+                Console.WriteLine("Автомобиль не движется.Нет топлива");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            if (amount > 0)
+            {
+                fuel += amount;
+                return true;
+            }
+            return false;
+
+        }  
+       
+    }
+    public class SportsCar: Car
+    {
+        public SportsCar(double initialFuel, double consumption) : base(initialFuel, consumption)
+        {
+        }
+    }
+    public class Truck : Car
+    {
+        public Truck(double initialFuel, double consumption) : base(initialFuel, consumption)
+        {
+        }
+    }
+
+
+}
