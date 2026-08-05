a1286a1b75a50526dda0ba4eb536612a900b0b0b
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2026-08-05 16:48:19 +0300

    фикс

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConsoleApp1
+{
+   public class Person(int age,string name,string email)
+    {
+        public int Age {  get; set; } = age;
+
+        public string Name { get; set; } = name;
+
+        public string Email { get; set; } = email;
+    }
+}
